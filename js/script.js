80f8a3f1c27afc000ff99f072cd1a1d5f3614352
Marco Romero <marco_antonio88_9@hotmail.com>
2017-02-08 12:53:00 +0100

    Project Design DIW.

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$("#menuMensajes").on("click", function(){
+  alert("Ha seleccionado Mensajes.");
+});
+
+$("#menuMuro").on("click", function(){
+  alert("Ha seleccionado Muro.");
+});
+
+$("#menuEventos").on("click", function(){
+  alert("Ha seleccionado Eventos.");
+});
+
+$("#menuFotos").on("click", function(){
+  alert("Ha seleccionado Fotos.");
+});
+
+$("#menuNoticias").on("click", function(){
+  alert("Ha seleccionado Últimas Noticias.");
+});
