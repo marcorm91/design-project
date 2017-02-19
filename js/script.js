889067422f566c026a3c6da8d5d027fd3448e9fa
Marco Romero <marco_antonio88_9@hotmail.com>
2017-02-19 19:19:59 +0100

    Añado info y precios sobre las modalidades de la academia.  Finalizado.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
 	
 $("#modal_a1").hide();
+$("#modal_a2").hide();
+$("#modal_b1").hide();
+$("#modal_b2").hide();
+$("#modal_c1").hide();
+$("#modal_c2").hide();
 
 	$("#info_a1").on("click", function(){
-	 
 		$("#modal_a1").dialog({
 					closeOnEscape: false,
 					open: function(event, ui) 
@@ -25,9 +29,132 @@ $("#modal_a1").hide();
 		        		}
 		    		}
 		});
-
 		$("body").css("overflow", "hidden");
+	});
 
+	$("#info_a2").on("click", function(){
+		$("#modal_a2").dialog({
+					closeOnEscape: false,
+					open: function(event, ui) 
+						{ 
+							$(".ui-dialog-titlebar-close", ui.dialog).hide(); 
+						},
+				    modal: true,
+				    draggable: false,
+				    resizable: false,
+				    show: { effect: "blind", duration: 800 },
+    				hide: 'blind',
+				    width: $(window).width()-20,
+				    height: $(window).height()-20,
+				    buttons: {
+		        		"CERRAR": function() {
+		        			$("body").css("overflow", "auto");
+
+		            		$(this).dialog("close");
+		        		}
+		    		}
+		});
+		$("body").css("overflow", "hidden");
+	});
+
+	$("#info_b1").on("click", function(){
+		$("#modal_b1").dialog({
+					closeOnEscape: false,
+					open: function(event, ui) 
+						{ 
+							$(".ui-dialog-titlebar-close", ui.dialog).hide(); 
+						},
+				    modal: true,
+				    draggable: false,
+				    resizable: false,
+				    show: { effect: "blind", duration: 800 },
+    				hide: 'blind',
+				    width: $(window).width()-20,
+				    height: $(window).height()-20,
+				    buttons: {
+		        		"CERRAR": function() {
+		        			$("body").css("overflow", "auto");
+
+		            		$(this).dialog("close");
+		        		}
+		    		}
+		});
+		$("body").css("overflow", "hidden");
+	});
+
+	$("#info_b2").on("click", function(){
+		$("#modal_b2").dialog({
+					closeOnEscape: false,
+					open: function(event, ui) 
+						{ 
+							$(".ui-dialog-titlebar-close", ui.dialog).hide(); 
+						},
+				    modal: true,
+				    draggable: false,
+				    resizable: false,
+				    show: { effect: "blind", duration: 800 },
+    				hide: 'blind',
+				    width: $(window).width()-20,
+				    height: $(window).height()-20,
+				    buttons: {
+		        		"CERRAR": function() {
+		        			$("body").css("overflow", "auto");
+
+		            		$(this).dialog("close");
+		        		}
+		    		}
+		});
+		$("body").css("overflow", "hidden");
+	});
+
+	$("#info_c1").on("click", function(){
+		$("#modal_c1").dialog({
+					closeOnEscape: false,
+					open: function(event, ui) 
+						{ 
+							$(".ui-dialog-titlebar-close", ui.dialog).hide(); 
+						},
+				    modal: true,
+				    draggable: false,
+				    resizable: false,
+				    show: { effect: "blind", duration: 800 },
+    				hide: 'blind',
+				    width: $(window).width()-20,
+				    height: $(window).height()-20,
+				    buttons: {
+		        		"CERRAR": function() {
+		        			$("body").css("overflow", "auto");
+
+		            		$(this).dialog("close");
+		        		}
+		    		}
+		});
+		$("body").css("overflow", "hidden");
+	});
+
+	$("#info_c2").on("click", function(){
+		$("#modal_c2").dialog({
+					closeOnEscape: false,
+					open: function(event, ui) 
+						{ 
+							$(".ui-dialog-titlebar-close", ui.dialog).hide(); 
+						},
+				    modal: true,
+				    draggable: false,
+				    resizable: false,
+				    show: { effect: "blind", duration: 800 },
+    				hide: 'blind',
+				    width: $(window).width()-20,
+				    height: $(window).height()-20,
+				    buttons: {
+		        		"CERRAR": function() {
+		        			$("body").css("overflow", "auto");
+
+		            		$(this).dialog("close");
+		        		}
+		    		}
+		});
+		$("body").css("overflow", "hidden");
 	});
 
 });
