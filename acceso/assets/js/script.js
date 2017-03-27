655a87276d42dc01662efad13fcc2fe9f50a8285
Marco Romero <marco_antonio88_9@hotmail.com>
2017-03-28 00:41:11 +0200

    Añadiendo una opacidad y la desactivación de elementos en el panel cuando abrimos el botón de menu superior desde móvil.

--- acceso/assets/js/script.js
+++ acceso/assets/js/script.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
 
     $("#fechaActual").append((new Date).getFullYear());
     
+    $("#boton-collapse").on("click", function(){
+        if ($("#boton-collapse").attr("aria-expanded") === "false"){
+            $(".container").css("opacity", 0.2);
+            $(".container").css("pointer-events", "none");
+        }else 
+            if($("#boton-collapse").attr("aria-expanded") === "true"){
+                $(".container").css("opacity", 1);
+                $(".container").css("pointer-events", "auto");
+            }
+    });
+    
 });
