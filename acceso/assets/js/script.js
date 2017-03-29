841a5ddd22c3f1d4d6be8e3f50fcab8867f9dc14
Marco Romero <marco_antonio88_9@hotmail.com>
2017-03-29 19:11:04 +0200

    Añadiendo fecha actual al registro del profesor para el campo fecha de registro.

--- acceso/assets/js/script.js
+++ acceso/assets/js/script.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
 
+    // Capturamos el año actual para adaptarlo al copyright del footer.
     $("#fechaActual").append((new Date).getFullYear());
     
+    // Comprobamos que la fecha lleve un 0 delante en el día o mes cuando éstos son < 10.
+    if((new Date).getDate() < 10){
+        $("#fecha-alta-profesor").val("0" + (new Date).getDate() + "/" + ((new Date).getMonth()+1) + "/" + (new Date).getFullYear());
+    }else 
+        if((new Date).getMonth() < 10){
+             $("#fecha-alta-profesor").val((new Date).getDate() + "/0" + ((new Date).getMonth()+1) + "/" + (new Date).getFullYear());
+        }else
+            if((new Date).getDate() < 10 && (new Date).getMonth() < 10){
+               $("#fecha-alta-profesor").val("0" + (new Date).getDate() + "/0" + ((new Date).getMonth()+1) + "/" + (new Date).getFullYear()); 
+            }
+    
+    // Cuando boton-collapse que es el botón de menú en dispositivo mobile se abre, no le daremos ninguna funcionalidad
+    // a los elementos del body.
     $("#boton-collapse").on("click", function(){
         if ($("#boton-collapse").attr("aria-expanded") === "false"){
             $(".container").css("opacity", 0.2);
