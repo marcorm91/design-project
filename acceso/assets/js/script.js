b8a430dc1218e48ee61fab3b800d07d3f4770e10
Marco Romero <marco_antonio88_9@hotmail.com>
2017-04-02 00:06:06 +0200

    Show y hide para el panel de búsqueda sobre el alumno.  Además, corrección de condiciones en fechas.

--- acceso/assets/js/script.js
+++ acceso/assets/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     
     // Ocultar subpaneles de búsqueda tras la carga inicial de la página.
     $("#subpanel-busqueda-profesor").hide();
+    $("#subpanel-busqueda-alumno").hide();
 
     // Capturamos el año actual para adaptarlo al copyright del footer.
     $("#fechaActual").append((new Date).getFullYear());
@@ -9,18 +10,20 @@ $(document).ready(function() {
     /*** MODALES DE REGISTRO ***/
         
     // Comprobamos que la fecha lleve un 0 delante en el día o mes cuando éstos son < 10.
-    if((new Date).getDate() < 10){
-        $("#fecha-alta-profesor, #fecha-alta-alumno, #fecha-alta-noticiario, #fecha-alta-gestor").
-        val("0" + (new Date).getDate() + "/" + ((new Date).getMonth()+1) + "/" + (new Date).getFullYear());
-    }else 
+    if((new Date).getDate() < 10 && (new Date).getMonth() < 10){
+       $("#fecha-alta-profesor, #fecha-alta-alumno, #fecha-alta-noticiario, #fecha-alta-gestor").
+       val("0" + (new Date).getDate() + "/0" + ((new Date).getMonth()+1) + "/" + (new Date).getFullYear()); 
+    }else
         if((new Date).getMonth() < 10){
-             $("#fecha-alta-profesor, #fecha-alta-alumno, #fecha-alta-noticiario, #fecha-alta-gestor").
-             val((new Date).getDate() + "/0" + ((new Date).getMonth()+1) + "/" + (new Date).getFullYear());
-        }else
-            if((new Date).getDate() < 10 && (new Date).getMonth() < 10){
-               $("#fecha-alta-profesor, #fecha-alta-alumno, #fecha-alta-noticiario, #fecha-alta-gestor").
-               val("0" + (new Date).getDate() + "/0" + ((new Date).getMonth()+1) + "/" + (new Date).getFullYear()); 
+            $("#fecha-alta-profesor, #fecha-alta-alumno, #fecha-alta-noticiario, #fecha-alta-gestor").
+            val("0" + (new Date).getDate() + "/" + ((new Date).getMonth()+1) + "/" + (new Date).getFullYear());
+        }else 
+            if((new Date).getDate() < 10){
+                 $("#fecha-alta-profesor, #fecha-alta-alumno, #fecha-alta-noticiario, #fecha-alta-gestor").
+                 val((new Date).getDate() + "/0" + ((new Date).getMonth()+1) + "/" + (new Date).getFullYear());
             }
+    
+            
         
     $("#fecha-nacimiento-profesor, #fecha-nacimiento-alumno").datepicker();
     
@@ -40,16 +43,21 @@ $(document).ready(function() {
     });
     
     /*** SUBPANELES DE BÚSQUEDA ***/
-    $("#busqueda-profesor").on("click", function(){
-               
+    $("#busqueda-profesor").on("click", function(){     
         $(".panel-opciones").hide();
         $(".panel-principal > h1").replaceWith("<h1><a href='../principal-gestor.html'>Panel Principal</a> "+
                                                "/ <a href='./buscar-elemento.html'> Buscar elemento </a>"+
                                                "/ Búsqueda de profesor </h1>");
         $("#subpanel-busqueda-profesor").show();
-        
     });
     
+    $("#busqueda-alumno").on("click", function(){
+        $(".panel-opciones").hide();
+        $(".panel-principal > h1").replaceWith("<h1><a href='../principal-gestor.html'>Panel Principal</a> "+
+                                               "/ <a href='./buscar-elemento.html'> Buscar elemento </a>"+
+                                               "/ Búsqueda de alumno </h1>");
+        $("#subpanel-busqueda-alumno").show();
+    });
     
     
 });
